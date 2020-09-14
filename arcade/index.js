6e708a3a3f8342a97a2f429f88ef0b29b42835e2
Matt Nawrocki <mjnrock@umich.edu>
2020-09-13 20:32:09 -0400

    init

--- arcade/index.js
+++ arcade/index.js
@@ -1,54 +1,40 @@
 import HID from "node-hid";
 import { Bitwise } from "@lespantsfancy/hive/lib/ext/Helper";
 
-const HIDDevices = HID.devices();
+// const HIDDevices = HID.devices();
+
 const DragonRise = new HID.HID(121, 6);
-
-
 DragonRise.setNonBlocking(true);
 DragonRise.on("data", buffer => {
     const [ lr, ud,,,, row1, row2 ] = [ ...buffer ];
-    const DIRECTIONS = {
-        UP: 255,
-        DOWN: 0,
-        LEFT: 255,
-        RIGHT: 0,
+    const mask = {
+        btn_1: Bitwise.has(row1, 2 << 3),
+        btn_2: Bitwise.has(row1, 2 << 4),
+        btn_3: Bitwise.has(row1, 2 << 5),
+
+        btn_4: Bitwise.has(row2, 2 << 0),
+        btn_5: Bitwise.has(row2, 2 << 1),
+        btn_6: Bitwise.has(row2, 2 << 2),
+
+        btn_start: Bitwise.has(row2, 2 << 4),
+
+        dir_left: lr === 255,
+        dir_right: lr ===0,
+        dir_up: ud === 255,
+        dir_down: ud === 0,
     };
 
-    const Mask = {
-        b1: Bitwise.has(row1, 2 << 3),
-        b2: Bitwise.has(row1, 2 << 4),
-        b3: Bitwise.has(row1, 2 << 5),
-
-        b4: Bitwise.has(row2, 2 << 0),
-        b5: Bitwise.has(row2, 2 << 1),
-        b6: Bitwise.has(row2, 2 << 2),
-
-        start: Bitwise.has(row2, 2 << 4),
-
-        left: lr === DIRECTIONS.LEFT,
-        right: lr === DIRECTIONS.RIGHT,
-        up: ud === DIRECTIONS.UP,
-        down: ud === DIRECTIONS.DOWN,
-        
-        direction: [],
-    };
-
-    if(Mask.left) {
-        Mask.direction.push("left");
-    }
-    if(Mask.right) {
-        Mask.direction.push("right");
-    }
-    if(Mask.up) {
-        Mask.direction.push("up");
-    }
-    if(Mask.down) {
-        Mask.direction.push("down");
+    const active = [];
+    for(let key in mask) {
+        if(mask[ key ] === true) {
+            active.push(key);
+        }
     }
 
-
-    console.log(JSON.stringify(Mask));
-
-    // console.log(JSON.stringify([ ...buffer ]));
+    if(active.length) {
+        console.clear();
+        console.log(active.join(" | "));
+    } else {
+        console.clear();
+    }
 });
