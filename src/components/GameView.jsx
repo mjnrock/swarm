f140de97c7c40545904ecd9c37ce68d89d14a66f
Matt Nawrocki <mjnrock@umich.edu>
2020-09-20 19:21:18 -0400

    init

--- src/components/GameView.jsx
+++ src/components/GameView.jsx
@@ -4,7 +4,7 @@ import Canvas from "./GameCanvas";
 
 export default function GameView({ game, canvasProps = {}, style = {}, ...rest } = {}) {
     return (
-        <div id="game-view" { ...rest } style={{ ...style }}>
+        <div id="game-view" { ...rest } style={{ textAlign: "center", ...style }}>
             <Canvas src={ game.state.view.current.camera.canvas } { ...canvasProps } />
         </div>
     );
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,13 +1,30 @@
 /* eslint-disable */
 import React from "react";
-import { Segment } from "semantic-ui-react";
+import { Segment, Header } from "semantic-ui-react";
 
 import ViewManager from "./../components/ViewManager";
 
 export default function Home(props) {
+    const subheader = [
+        "Experienced Players Only",
+        "A Game of Shtrategery",
+        "Find your hole!",
+        "If I had a nickle...",
+        "Beware of Shnakes!",
+        "Horsefeathers!",
+    ];
+
     return (
         <Segment>
+            <Header as="h1" textAlign="center" style={{ fontFamily: "Freckle Face", fontSize: "5em" }}>Shnake!</Header>
+            <Header as="h2" textAlign="center" style={{ fontFamily: "Freckle Face", fontWeight: 400 }}>{ subheader[ ~~(Math.random() * subheader.length) ] }</Header>
+
             <ViewManager />
+
+            <div style={{ fontFamily: "PressStart2PRegular", fontSize: "1.2em" }}>Player 1: 0</div>
+            <div style={{ fontFamily: "PressStart2PRegular", fontSize: "1.2em" }}>Player 2: 0</div>
+            <div style={{ fontFamily: "PressStart2PRegular", fontSize: "1.2em" }}>Player 3: 0</div>
+            <div style={{ fontFamily: "PressStart2PRegular", fontSize: "1.2em" }}>Player 4: 0</div>
         </Segment>
     )
 }
